4ceb8d3223e4341203fec46cb43335297449d5a2
Jorge Carrazco <48524432+Jcarrazco@users.noreply.github.com>
2019-07-16 21:28:05 -0500

    Add files via upload

---
+++ Mergesort/Mergesort/Program.cs
@@ -0,0 +1,107 @@
+﻿/*
+ THIS CODE ID PROPERTY OF https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-7.php
+ this is a representation with educational propose
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mergesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+
+            Random random = new Random();
+
+            Console.WriteLine("Original array elements:");
+            for (int i = 0; i < 10; i++)
+            {
+                unsorted.Add(random.Next(0, 100));
+                Console.Write(unsorted[i] + " ");
+            }
+            Console.WriteLine();
+            //Genero una lista de 10 elementos al azar para ordenar
+
+            sorted = MergeSort(unsorted);
+
+
+            //imprime la nueva lista ordenada
+            Console.WriteLine("Sorted array elements: ");
+            foreach (int x in sorted)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("\n");
+            Console.ReadKey();
+        }
+
+
+        private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            //crea listas auxiliares para dividir la lista original
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            //aproximadamente la mitad de elementos para cada sublista
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            //Recursividad de metodo para generar listas mas pequeñas cada vez
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right); // llama a la mezcla de estas sub listas
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+    }
+
+}
